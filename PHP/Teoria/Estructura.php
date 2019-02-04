108a1662a3ebbcd8ee9faa87c03ea567c60b3a0b
AdolfoAlexei <adcaga96@gmail.com>
2019-02-04 14:32:39 +0100

    php

--- PHP/Teoria/Estructura.php
+++ PHP/Teoria/Estructura.php
@@ -9,68 +9,9 @@
 <body>
 
 <?php
-for ($i=0; $i < 6; $i = $i + 2) {   //El bucle iria de dos en dos
-    print "$i\n";
-}
-?>  
-</br></br>
-<?php
-for ($i=6; $i >= 0; $i--) {   //El blucle iria del 6 al 0
-    print "$i\n";
-}
+
+
 ?>
-
-
-<?php
-for ($i=6; $i >= 0; $i--) {   // Muestra el 6, 4, 2 y 0.
-    if ($i % 2 == 0) {
-    print "<p>$i</>";
-    }
-}
-?>
-
-<?php
-$contador = 0;
-for ($i=6; $i >= 0; $i--) {   // Contador de numeros impares entre 6 y 0
-    if ($i % 2 == 0) {
-        print "<p>$i</p>";
-    } else {
-        $contador++;
-    }
-}
-echo "Hay $contador impares entre 6 y 0";
-?>
-</br></br></br>
-
-
-<?php
-$columnas = 4;
-$filas    = 3;
-
-print "<table border=\"1\">\n";            // Abre la tabla
-print "  <caption>Tabla</caption>\n";      // Crea la leyenda <caption>
-print "  <tbody>\n";                       // Abre el cuerpo de tabla <tbody>
-
-print "    <tr>\n";                        // Abre la primera fila
-print "      <th></th>\n";                 // Crea la primera celda <th> de la primera fila (sin número)
-for ($j = 1; $j <= $columnas; $j++) {      // Bucle 1 se ejecuta tantas veces como columnas tenga la tabla
-    print "      <th>$j</th>\n";           // Crea las celdas <th> de la primera fila (con número)
-}
-print "    </tr>\n";                       // Cierra la primera fila
-
-for ($i = 1; $i <= $filas; $i++) {         // Bucle 2 (genera el resto de filas de la tabla)
-    print "    <tr>\n";                    // Abre la fila
-    print "      <th>$i</th>\n";           // Crea la primera celda <th> de cada fila (con número)
-    for ($j = 1; $j <= $columnas; $j++) {  // Bucle 3 se ejecuta tantas veces como columnas tenga la tabla
-        print "      <td>$i-$j</td>\n";     // Crea el resto de celdas <td> de cada fila (con números)
-    }
-    print "    </tr>\n";                   // Cierra la fila
-}
-
-print "  </tbody>\n";                      // Cierra el cuerpo de tabla <tbody>
-print "</table>\n";                        // Cierra la tabla
-?>
-
 
 
 
